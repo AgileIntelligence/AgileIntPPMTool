8634f89adbfe00ff729e663cb8b1d25c0724a2c9
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-22 12:25:31 -0400

    first commit

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+}
---
+++ PPMToolFullStack/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package io.agileintelligence.ppmtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmtoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
