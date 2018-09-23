c3c0ce8fe316e1f1b6a6e4596d07e08a12e18912
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-23 14:48:56 -0400

    added dashboar component react /  no java changes

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to PPMTool</h1>
-        </header>
+        <Dashboard />
+        <Dashboard />
+        <Dashboard />
       </div>
     );
   }
---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return <h1>Welcome to the Dashboard</h1>;
+  }
+}
+
+export default Dashboard;
