51d62e9d9d0ad86f75872cde36ea568afe1dfd22
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-23 15:47:52 -0400

    added bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
         <ProjectItem />
         <ProjectItem />
