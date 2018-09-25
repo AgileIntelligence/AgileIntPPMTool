f4c8f34aef6d15e4d2d43346a51b1be12362f536
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-25 16:10:27 -0400

    asd

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -4,7 +4,7 @@ class AddProject extends Component {
   render() {
     return (
       <div>
-        <h1>Hello branch17!</h1>
+        <h1>Hello branch17!!</h1>
         <h1>Add Project Form</h1>
       </div>
     );
