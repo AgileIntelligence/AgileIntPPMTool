23cf8353edd7fff9ba5794c09956301056e216e4
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-25 20:06:02 -0400

    create redux store

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,17 +5,21 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ ppmtool-react-client/src/store.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initalState = {};
+const middleware = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(applyMiddleware(...middleware))
+  );
+}
+
+export default store;
