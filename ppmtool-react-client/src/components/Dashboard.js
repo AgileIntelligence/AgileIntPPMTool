3e53bc8113df22805a28fa1e2e836d030f10844b
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-26 21:28:27 -0400

    branch22

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -36,11 +36,11 @@ Dashboard.propTypes = {
   getProjects: PropTypes.func.isRequired
 };
 
-// const mapStateToProps = state => ({
-//   project: state.project
-// });
+const mapStateToProps = state => ({
+  project: state.project
+});
 
 export default connect(
-  null,
+  mapStateToProps,
   { getProjects }
 )(Dashboard);
