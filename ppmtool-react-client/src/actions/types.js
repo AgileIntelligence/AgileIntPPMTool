2280f1ab00e88948d841d8cc3490f8f6a1ab5548
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-30 12:10:39 -0400

    types and reducers for project task

--- ppmtool-react-client/src/actions/types.js
+++ ppmtool-react-client/src/actions/types.js
@@ -2,3 +2,9 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+//Types for BACKLOG ACTIONS
+
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
---
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -0,0 +1,36 @@
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from "../actions/projectActions";
+
+const initialState = {
+  project_tasks: [],
+  project_task: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        project_tasks: action.payload
+      };
+
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload
+      };
+
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state
+
+        // TO_DO
+      };
+
+    default:
+      return state;
+  }
+}
